cfc3161505e931c362c491957dbffb1901de48e2
Eric West <eric@makechip.com>
2016-04-08 11:35:38 -0700

    backup

---
+++ src/makechip/Stdf.d
@@ -0,0 +1,38 @@
+import std.stdio;
+import std.range;
+import std.array;
+import std.algorithm;
+
+class StdfReader
+{
+    private InputStreamRange src;
+    public const string filename;
+    
+    this(string filename, size_t bufferSize)
+    {
+        this.filename = filename;
+        auto f = new File(filename, "rb");
+        if (f.size() > bufferSize) src = new FileBinaryInputStream(filename, bufferSize);
+        else src = new FastFileBinaryInputStream(filename, bufferSize);
+    }
+
+    void read(void)
+    {
+        while (!src.empty)
+        {
+            ubyte a = src.front; src.popFront();
+            if (src.empty) throw new Exception("Unexpected EOF: " ~ filename);
+            ubyte b = src.front; src.popFront();
+            if (src.empty) throw new Exception("Unexpected EOF: " ~ filename);
+            ubyte c = src.front; src.popFront();
+            if (src.empty) throw new Exception("Unexpected EOF: " ~ filename);
+            ubyte d = src.front; src.popFront();
+            if (src.empty) throw new Exception("Unexpected EOF: " ~ filename);
+            short recordType = cast(short) 0;
+            short subType = cast(short) 0;
+            recordType |= (c & 0xFF);
+            subType |= (d & 0xFF);
+
+    }
+
+}
