03edb6da509dd334e2554811ee85fab4e1784fe6
Eric West <eric.west@itestinc.com>
2019-12-26 17:42:12 -0800

    backup

--- src/makechip/Spreadsheet.d
+++ src/makechip/Spreadsheet.d
@@ -126,5 +126,48 @@ public void genSpreadsheet(CmdOptions options, StdfDB stdfdb)
             }
             if (devices[$-1].devId != prevDevice.devId) devices ~= prevDevice;
         }
-   } 
+        if (options.flowAnalysis)
+        {
+            size_t maxLen = 0;
+            size_t maxLoc = 0;
+            size_t i = 0;
+            foreach (d; devices)
+            {
+                if (d.tests.length > maxLen)
+                {
+                    maxLen = d.tests.length;
+                    maxLoc = i;
+                }
+                i++;
+            }
+            // First expand the test list so they are essentially equal length (assuming they are passing devices)
+            TestRecord[][] newTests = new TestRecord[][devices.length];
+            for (size_t j=0; j<devices[maxLoc].tests.length; j++)
+            {
+                scan(j, devices[maxLoc].tests[j].id, devices[maxLoc].tests[j].type, devices, newTests);
+            }
+        }
+    } 
+}
+
+private void scan(size_t tnum, const TestID id, const TestType type, DeviceResult[] devices, TestRecord[][] newTests)
+{
+    bool diff = false;
+    TestID nextId;
+    TestType nextType;
+    for (size_t i=0; i<devices.length; i++)
+    {
+        if (devices[i].tests[tnum].id != id || devices[i].tests[tnum].type != type)
+        {
+            diff = true;
+            newTests[i] ~= null;
+            nextId = cast(TestID) devices[i].tests[tnum].id;
+            nextType = devices[i].tests[tnum].type;
+        }
+        else 
+        {
+            newTests[i] ~= devices[i].tests[tnum];
+        }
+    }
+    if (diff) scan(tnum, nextId, nextType, devices, newTests);
 }
