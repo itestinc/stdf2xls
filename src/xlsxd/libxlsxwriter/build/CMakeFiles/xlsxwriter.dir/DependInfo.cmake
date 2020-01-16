cd8305c5742edde1d4602117c3f2167d3e6be025
Eric West <eric.west@itestinc.com>
2020-01-15 17:36:49 -0800

    backup

---
+++ src/xlsxd/libxlsxwriter/build/CMakeFiles/xlsxwriter.dir/DependInfo.cmake
@@ -0,0 +1,51 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/eric/.dub/packages/xlsxd-2.2.0/xlsxd/libxlsxwriter/src/app.c" "/home/eric/.dub/packages/xlsxd-2.2.0/xlsxd/libxlsxwriter/build/CMakeFiles/xlsxwriter.dir/src/app.c.o"
+  "/home/eric/.dub/packages/xlsxd-2.2.0/xlsxd/libxlsxwriter/src/chart.c" "/home/eric/.dub/packages/xlsxd-2.2.0/xlsxd/libxlsxwriter/build/CMakeFiles/xlsxwriter.dir/src/chart.c.o"
+  "/home/eric/.dub/packages/xlsxd-2.2.0/xlsxd/libxlsxwriter/src/chartsheet.c" "/home/eric/.dub/packages/xlsxd-2.2.0/xlsxd/libxlsxwriter/build/CMakeFiles/xlsxwriter.dir/src/chartsheet.c.o"
+  "/home/eric/.dub/packages/xlsxd-2.2.0/xlsxd/libxlsxwriter/src/comment.c" "/home/eric/.dub/packages/xlsxd-2.2.0/xlsxd/libxlsxwriter/build/CMakeFiles/xlsxwriter.dir/src/comment.c.o"
+  "/home/eric/.dub/packages/xlsxd-2.2.0/xlsxd/libxlsxwriter/src/content_types.c" "/home/eric/.dub/packages/xlsxd-2.2.0/xlsxd/libxlsxwriter/build/CMakeFiles/xlsxwriter.dir/src/content_types.c.o"
+  "/home/eric/.dub/packages/xlsxd-2.2.0/xlsxd/libxlsxwriter/src/core.c" "/home/eric/.dub/packages/xlsxd-2.2.0/xlsxd/libxlsxwriter/build/CMakeFiles/xlsxwriter.dir/src/core.c.o"
+  "/home/eric/.dub/packages/xlsxd-2.2.0/xlsxd/libxlsxwriter/src/custom.c" "/home/eric/.dub/packages/xlsxd-2.2.0/xlsxd/libxlsxwriter/build/CMakeFiles/xlsxwriter.dir/src/custom.c.o"
+  "/home/eric/.dub/packages/xlsxd-2.2.0/xlsxd/libxlsxwriter/src/drawing.c" "/home/eric/.dub/packages/xlsxd-2.2.0/xlsxd/libxlsxwriter/build/CMakeFiles/xlsxwriter.dir/src/drawing.c.o"
+  "/home/eric/.dub/packages/xlsxd-2.2.0/xlsxd/libxlsxwriter/src/format.c" "/home/eric/.dub/packages/xlsxd-2.2.0/xlsxd/libxlsxwriter/build/CMakeFiles/xlsxwriter.dir/src/format.c.o"
+  "/home/eric/.dub/packages/xlsxd-2.2.0/xlsxd/libxlsxwriter/src/hash_table.c" "/home/eric/.dub/packages/xlsxd-2.2.0/xlsxd/libxlsxwriter/build/CMakeFiles/xlsxwriter.dir/src/hash_table.c.o"
+  "/home/eric/.dub/packages/xlsxd-2.2.0/xlsxd/libxlsxwriter/src/packager.c" "/home/eric/.dub/packages/xlsxd-2.2.0/xlsxd/libxlsxwriter/build/CMakeFiles/xlsxwriter.dir/src/packager.c.o"
+  "/home/eric/.dub/packages/xlsxd-2.2.0/xlsxd/libxlsxwriter/src/relationships.c" "/home/eric/.dub/packages/xlsxd-2.2.0/xlsxd/libxlsxwriter/build/CMakeFiles/xlsxwriter.dir/src/relationships.c.o"
+  "/home/eric/.dub/packages/xlsxd-2.2.0/xlsxd/libxlsxwriter/src/shared_strings.c" "/home/eric/.dub/packages/xlsxd-2.2.0/xlsxd/libxlsxwriter/build/CMakeFiles/xlsxwriter.dir/src/shared_strings.c.o"
+  "/home/eric/.dub/packages/xlsxd-2.2.0/xlsxd/libxlsxwriter/src/styles.c" "/home/eric/.dub/packages/xlsxd-2.2.0/xlsxd/libxlsxwriter/build/CMakeFiles/xlsxwriter.dir/src/styles.c.o"
+  "/home/eric/.dub/packages/xlsxd-2.2.0/xlsxd/libxlsxwriter/src/theme.c" "/home/eric/.dub/packages/xlsxd-2.2.0/xlsxd/libxlsxwriter/build/CMakeFiles/xlsxwriter.dir/src/theme.c.o"
+  "/home/eric/.dub/packages/xlsxd-2.2.0/xlsxd/libxlsxwriter/src/utility.c" "/home/eric/.dub/packages/xlsxd-2.2.0/xlsxd/libxlsxwriter/build/CMakeFiles/xlsxwriter.dir/src/utility.c.o"
+  "/home/eric/.dub/packages/xlsxd-2.2.0/xlsxd/libxlsxwriter/src/vml.c" "/home/eric/.dub/packages/xlsxd-2.2.0/xlsxd/libxlsxwriter/build/CMakeFiles/xlsxwriter.dir/src/vml.c.o"
+  "/home/eric/.dub/packages/xlsxd-2.2.0/xlsxd/libxlsxwriter/src/workbook.c" "/home/eric/.dub/packages/xlsxd-2.2.0/xlsxd/libxlsxwriter/build/CMakeFiles/xlsxwriter.dir/src/workbook.c.o"
+  "/home/eric/.dub/packages/xlsxd-2.2.0/xlsxd/libxlsxwriter/src/worksheet.c" "/home/eric/.dub/packages/xlsxd-2.2.0/xlsxd/libxlsxwriter/build/CMakeFiles/xlsxwriter.dir/src/worksheet.c.o"
+  "/home/eric/.dub/packages/xlsxd-2.2.0/xlsxd/libxlsxwriter/src/xmlwriter.c" "/home/eric/.dub/packages/xlsxd-2.2.0/xlsxd/libxlsxwriter/build/CMakeFiles/xlsxwriter.dir/src/xmlwriter.c.o"
+  "/home/eric/.dub/packages/xlsxd-2.2.0/xlsxd/libxlsxwriter/third_party/md5/md5.c" "/home/eric/.dub/packages/xlsxd-2.2.0/xlsxd/libxlsxwriter/build/CMakeFiles/xlsxwriter.dir/third_party/md5/md5.c.o"
+  "/home/eric/.dub/packages/xlsxd-2.2.0/xlsxd/libxlsxwriter/third_party/minizip/ioapi.c" "/home/eric/.dub/packages/xlsxd-2.2.0/xlsxd/libxlsxwriter/build/CMakeFiles/xlsxwriter.dir/third_party/minizip/ioapi.c.o"
+  "/home/eric/.dub/packages/xlsxd-2.2.0/xlsxd/libxlsxwriter/third_party/minizip/zip.c" "/home/eric/.dub/packages/xlsxd-2.2.0/xlsxd/libxlsxwriter/build/CMakeFiles/xlsxwriter.dir/third_party/minizip/zip.c.o"
+  "/home/eric/.dub/packages/xlsxd-2.2.0/xlsxd/libxlsxwriter/third_party/tmpfileplus/tmpfileplus.c" "/home/eric/.dub/packages/xlsxd-2.2.0/xlsxd/libxlsxwriter/build/CMakeFiles/xlsxwriter.dir/third_party/tmpfileplus/tmpfileplus.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "NOCRYPT"
+  "NOUNCRYPT"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  "../include/xlsxwriter"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
