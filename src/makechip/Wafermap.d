37c1af28d7be2138998cb17c397fe5e95a5a3fdf
Jane Han <jane.han@itestinc.com>
2020-02-14 09:28:36 -0800

    Backup

--- src/makechip/Wafermap.d
+++ src/makechip/Wafermap.d
@@ -15,29 +15,11 @@ import makechip.logo;
 import makechip.Util;
 import makechip.SpreadsheetWriter;
 
-
+/**
+	Read from the STDF database to generate a wafer map in excel.
+*/
 public void genWafermap(CmdOptions options, StdfDB stdfdb, Config config)
 {
-	Workbook wb = newWorkbook("Wafermap.xlsx");
-	auto ws = wb.addWorksheet("Page 1");
-
-	lxw_image_options img_options;
-	const double ss_width = 449 * 0.350;
-	const double ss_height = 245 * 0.324;
-	img_options.x_scale = (4.0 * 70.0) / ss_width;
-	img_options.y_scale = (7.0 * 20.0) / ss_height;
-	ws.mergeRange(0, 0, 7, 3, null);
-	img_options.object_position = lxw_object_position.LXW_OBJECT_MOVE_AND_SIZE;
-	ws.insertImageBufferOpt(cast(uint) 0, cast(ushort) 0, img.dup.ptr, img.length, &img_options);
-	//ws.insertImageOpt(cast(uint) 0, cast(ushort) 0, "itest_logo.png", &img_options);
-
-	MultiMap!(Workbook, string, string) wbMap = new MultiMap!(Workbook, string, string)();
-
-	wbMap.put(wb, "asd", "lkj");
-
-	wb.close();
-
-
 	foreach(hdr; stdfdb.deviceMap.keys) {
 
 		uint[] hwbin;
@@ -46,14 +28,6 @@ public void genWafermap(CmdOptions options, StdfDB stdfdb, Config config)
 		hwbin.length = 0;
 		x_coord.length = 0;
 		y_coord.length = 0;
-
-		if (options.verbosityLevel > 0) {
-			// useful header values
-			writeln("hdr.wafer_id = ", hdr.wafer_id);
-			writeln("hdr.devName = ", hdr.devName);
-			writeln("hdr.temperature = ", hdr.temperature);
-			writeln("hdr.step = ", hdr.step);
-		}
 
 		foreach(i, dr; stdfdb.deviceMap[hdr]) {
 
@@ -65,62 +39,64 @@ public void genWafermap(CmdOptions options, StdfDB stdfdb, Config config)
 			x_coord[i] = dr.devId.id.xy.x;
 			y_coord[i] = dr.devId.id.xy.y;
 		}
-		// writeln("hwbin = ", hwbin);
-		// writeln("x_coord = ", x_coord);
-		// writeln("y_coord = ", y_coord);
 
-		// ASY Format
-		// if(options.asy) {
+		// sort for min/max elements 
+		int[] x_sorted = x_coord.dup;
+		int[] y_sorted = y_coord.dup;
+
+		import std.algorithm.sorting : sort;
+		x_sorted.sort();
+		y_sorted.sort();
+
+		const int x_min = x_sorted[0];
+		const int y_min = y_sorted[0];
+		const int x_max = x_sorted[$-1] - x_sorted[0];
+		const int y_max = y_sorted[$-1] - y_sorted[0];
+
+		// shift for indexing
+		int[] x_shifted = new int[x_coord.length];
+		int[] y_shifted = new int[y_coord.length];
+		x_shifted[] = x_coord[] - x_min;
+		y_shifted[] = y_coord[] - y_min;
+
+		// create empty map
+		const uint col = x_max + 1;
+		const uint row = y_max + 1;
+
+		char[][] matrix = new char[][](row, col);
+		writeln("row = ", row);
+		writeln("col = ", col);
+
+		// pre-fill map with dots
+		for(uint i = 0; i < row; i++) {
+			for(uint j = 0; j < col; j++) {
+				matrix[i][j] = '.';
+			}
+		}
+
+		uint goodbins = 0;
+		uint badbins = 0;
+		// fill map with hwbins
+		foreach(i, bin; hwbin) {
+			switch(hwbin[i]) {
+				default:
+					matrix[y_shifted[i]][x_shifted[i]] = '?'; badbins++; break;
+					// throw new Exception("Unsupported HW bin number");
+				case 1:
+					matrix[y_shifted[i]][x_shifted[i]] = '1'; goodbins++; break;
+				case 2:
+					matrix[y_shifted[i]][x_shifted[i]] = 'X'; badbins++; break;
+			}
+		}
+
 		if(options.asciiDump) {
-
-			// sort for min/max elements
-			int[] x_sorted = x_coord.dup;
-			int[] y_sorted = y_coord.dup;
-
-			import std.algorithm.sorting : sort;
-			x_sorted.sort();
-			y_sorted.sort();
-
-			const int x_min = x_sorted[0];
-			const int y_min = y_sorted[0];
-			const int x_max = x_sorted[$-1] - x_sorted[0];
-			const int y_max = y_sorted[$-1] - y_sorted[0];
-
-			// shift for indexing
-			int[] x_shifted = new int[x_coord.length];
-			int[] y_shifted = new int[y_coord.length];
-			x_shifted[] = x_coord[] - x_min;
-			y_shifted[] = y_coord[] - y_min;
-
-			// create empty map
-			const uint col = x_max + 1;
-			const uint row = y_max + 1;
-
-			char[][] matrix = new char[][](row, col);
-			writeln("row = ", row);
-			writeln("col = ", col);
-
-			// pre-fill map with dots
-			for(uint i = 0; i < row; i++) {
-				for(uint j = 0; j < col; j++) {
-					matrix[i][j] = '.';
-				}
-			}
-
-			uint goodbins = 0;
-			uint badbins = 0;
-			// fill map with hwbins
-			foreach(i, bin; hwbin) {
-				switch(hwbin[i]) {
-					default:
-						matrix[y_shifted[i]][x_shifted[i]] = '?'; badbins++; break;
-						// throw new Exception("Unsupported HW bin number");
-					case 1:
-						matrix[y_shifted[i]][x_shifted[i]] = '1'; goodbins++; break;
-					case 2:
-						matrix[y_shifted[i]][x_shifted[i]] = 'X'; badbins++; break;
-				}
-			}
+			// useful header values
+			writeln("hdr.wafer_id = ", hdr.wafer_id);
+			writeln("hdr.lot_id = ", hdr.lot_id);
+			writeln("hdr.sublot_id = ", hdr.sublot_id);
+			writeln("hdr.devName = ", hdr.devName);
+			writeln("hdr.temperature = ", hdr.temperature);
+			writeln("hdr.step = ", hdr.step);
 
 			// print map
 			writeln("good bins = ", goodbins);
@@ -130,19 +106,69 @@ public void genWafermap(CmdOptions options, StdfDB stdfdb, Config config)
 				writeln(n);
 			}
 
+			// rotate 90 and print
 			char[][] mat_rot = new char[][](col, row);
 			rotate90(matrix, mat_rot, row, col);
-
 			writeln("rotated:");
 			foreach(n; mat_rot) {
 				writeln(n);
 			}
 		}
+
+	// EXCEL
+		import std.algorithm: canFind;
+		string wfile = options.wfile;	// "<device>_<lot>_<wafer>"
+		const bool separateFileForDevice = canFind(wfile, "<device>");
+		const bool separateFileForLot = canFind(wfile, "<lot>");
+		const bool separateFileForWafer = canFind(wfile, "<wafer>");
+		
+		import std.array : replace;
+		string fname = replace(wfile, "<device>", hdr.devName).replace("<lot>", hdr.lot_id).replace("<wafer>", hdr.wafer_id);
+		writeln("fname = ", fname);
+
+		/*
+		if(separateFileForDevice && separateFileForLot && separateFileForWafer) {
+			import std.array : replace;
+			fname = replace(wfile, "<device>", hdr.devName).replace("<lot>", hdr.lot_id).replace("<wafer>", hdr.wafer_id);
+			writeln("fname = ", fname);
+		}
+		else {
+			// ...
+		}*/
+
+		Workbook wb = newWorkbook(fname);
+		auto ws = wb.addWorksheet("Page 1");
+
+		lxw_image_options img_options;
+		const double ss_width = 449 * 0.350;
+		const double ss_height = 245 * 0.324;
+		img_options.x_scale = (4.0 * 70.0) / ss_width;
+		img_options.y_scale = (7.0 * 20.0) / ss_height;
+		ws.mergeRange(0, 0, 7, 3, null);
+		img_options.object_position = lxw_object_position.LXW_OBJECT_MOVE_AND_SIZE;
+		ws.insertImageBufferOpt(cast(uint) 0, cast(ushort) 0, img.dup.ptr, img.length, &img_options);
+		//ws.insertImageOpt(cast(uint) 0, cast(ushort) 0, "itest_logo.png", &img_options);
+
+		import std.conv : to;
+		// ws.write(10, 10, "hello");
+		foreach(i, row_arr; matrix) {
+			foreach(j, val; row_arr) {
+
+				string val2 = to!string(val);
+				initFormats(wb, options, config);
+				ws.write(cast(uint) i, cast(ushort) j, val2);
+
+				// set column width to 2
+				// don't write '.'s.. maybe color gray
+				// add color
+				// center the text
+			}
+		}
+
+		wb.close();
+
 	}
 
-	if(options.asciiDump) {
-		// dump wafermap to ascii...
-	}
 }
 
 unittest {
@@ -160,7 +186,9 @@ unittest {
 	writeln("Unit test passes");
 }
 
-// O(n^2)
+/**
+	O(n^2)
+*/
 void transpose(char[][] a, char[][] b, uint row, uint col) {
 	for(uint i = 0; i < row; i++) {
 		for(uint j = 0; j < col; j++) {
@@ -169,13 +197,15 @@ void transpose(char[][] a, char[][] b, uint row, uint col) {
 	}
 }
 
+/**
+	rotate 90 clockwise:
+	1. transpose
+	2. reverse each row
+*/
 void rotate90(char[][] a, char[][] b, uint row, uint col) {
-	//transpose
 	transpose(a, b, row, col);
-
-	// reverse each row
-	const int new_row = col;
-	const int new_col = row;
+	const uint new_row = col;
+	const uint new_col = row;
 	char[][] tmp = new char[][](new_row,new_col);
 
 	foreach(x, rows; b) {
