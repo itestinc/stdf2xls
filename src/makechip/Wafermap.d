988b253391046773bb1ca7127cabcabe6d0ac577
Jane Han <jane.han@itestinc.com>
2020-02-15 01:03:35 -0800

    refactor for speed

--- src/makechip/Wafermap.d
+++ src/makechip/Wafermap.d
@@ -29,6 +29,7 @@ public void genWafermap(CmdOptions options, StdfDB stdfdb, Config config)
 		x_coord.length = 0;
 		y_coord.length = 0;
 
+		// retrieve data
 		foreach(i, dr; stdfdb.deviceMap[hdr]) {
 
 			hwbin.length +=1;
@@ -53,82 +54,29 @@ public void genWafermap(CmdOptions options, StdfDB stdfdb, Config config)
 		const int x_max = x_sorted[$-1] - x_sorted[0];
 		const int y_max = y_sorted[$-1] - y_sorted[0];
 
-		// shift for indexing
+		// shift for indexing from origin
 		int[] x_shifted = new int[x_coord.length];
 		int[] y_shifted = new int[y_coord.length];
 		x_shifted[] = x_coord[] - x_min;
 		y_shifted[] = y_coord[] - y_min;
 
-		// create empty map
+		// create empty 2D map
 		const uint col = x_max + 1;
 		const uint row = y_max + 1;
+		uint[][] matrix_uint = new uint[][](row,col);	// pre-filled with zeros
 
-		char[][] matrix = new char[][](row, col);		// inital val is 0xFF
-
-		uint[][] matrix_uint = new uint[][](row,col);
-
-
-		// pre-fill map with dots
-		for(uint i = 0; i < row; i++) {
-			for(uint j = 0; j < col; j++) {
-				matrix[i][j] = '.';
-
-				matrix_uint[i][j] = 0;
-			}
-		}
-
+		// fill map with hwbins
 		uint goodbins = 0;
 		uint badbins = 0;
-		// fill map with hwbins
 		foreach(i, bin; hwbin) {
-
 			matrix_uint[y_shifted[i]][x_shifted[i]] = bin;
 
-			switch(hwbin[i]) {
-				default:
-					matrix[y_shifted[i]][x_shifted[i]] = '?'; badbins++; break;
-					// throw new Exception("Unsupported HW bin number");
-				case 1:
-					matrix[y_shifted[i]][x_shifted[i]] = '1'; goodbins++; break;
-				case 2:
-					matrix[y_shifted[i]][x_shifted[i]] = 'X'; badbins++; break;
+			switch(bin) {
+				case 1: goodbins++; break;
+				default: badbins++; break;
 			}
 		}
 
-		// rotate 90
-		char[][] mat_rot = new char[][](col, row);
-		rotate90(matrix, mat_rot, row, col);
-
-		if(options.asciiDump) {
-			// useful header values
-			writeln("hdr.wafer_id = ", hdr.wafer_id);
-			writeln("hdr.lot_id = ", hdr.lot_id);
-			writeln("hdr.sublot_id = ", hdr.sublot_id);
-			writeln("hdr.devName = ", hdr.devName);
-			writeln("hdr.temperature = ", hdr.temperature);
-			writeln("hdr.step = ", hdr.step);
-			// print map
-			writeln("row = ", row);
-			writeln("col = ", col);
-			writeln("good bins = ", goodbins);
-			writeln("bad bins = ", badbins);
-			writeln("total bins = ", goodbins+badbins);
-			//foreach(n; matrix) {
-			//	writeln(n);
-			//}
-			// print rotated map
-			writeln("rotated 90 CW:");
-			foreach(n; mat_rot) {
-				writeln(n);
-			}
-
-			//writeln("matrix_uint:");
-			//foreach(n; matrix_uint) {
-			//	writeln(n);
-			//}
-		}
-
-	// EXCEL
 		import std.algorithm: canFind;
 		string wfile = options.wfile;	// "<device>_<lot>_<wafer>"
 		const bool separateFileForDevice = canFind(wfile, "<device>");
@@ -175,12 +123,23 @@ public void genWafermap(CmdOptions options, StdfDB stdfdb, Config config)
 		ws.write(11, 0, "devName:", headerNameFmt);
 		ws.write(12, 0, "temperature:", headerNameFmt);
 		ws.write(13, 0, "step:", headerNameFmt);
+		ws.write(14, 0, "row:", headerNameFmt);
+		ws.write(15, 0, "col:", headerNameFmt);
+		ws.write(16, 0, "good bins:", headerNameFmt);
+		ws.write(17, 0, "bad bins:", headerNameFmt);
+		ws.write(18, 0, "total bins:", headerNameFmt);
+
 		ws.write(8, 1, hdr.wafer_id, headerValueFmt);
 		ws.write(9, 1, hdr.lot_id, headerValueFmt);
 		ws.write(10, 1, hdr.sublot_id, headerValueFmt);
 		ws.write(11, 1, hdr.devName, headerValueFmt);
 		ws.write(12, 1, hdr.temperature, headerValueFmt);
 		ws.write(13, 1, hdr.step, headerValueFmt);
+		ws.write(14, 1, row, headerValueFmt);
+		ws.write(15, 1, col, headerValueFmt);
+		ws.write(16, 1, goodbins, headerValueFmt);
+		ws.write(17, 1, badbins, headerValueFmt);
+		ws.write(18, 1, (goodbins+badbins), headerValueFmt);
 
 		ws.mergeRange(8, 1, 8, 3, null);
 		ws.mergeRange(9, 1, 9, 3, null);
@@ -188,6 +147,26 @@ public void genWafermap(CmdOptions options, StdfDB stdfdb, Config config)
 		ws.mergeRange(11, 1, 11, 3, null);
 		ws.mergeRange(12, 1, 12, 3, null);
 		ws.mergeRange(13, 1, 13, 3, null);
+		ws.mergeRange(14, 1, 14, 3, null);
+		ws.mergeRange(15, 1, 15, 3, null);
+		ws.mergeRange(16, 1, 16, 3, null);
+		ws.mergeRange(17, 1, 17, 3, null);
+		ws.mergeRange(18, 1, 18, 3, null);
+
+		// dump in ASE format
+		if(options.asciiDump) {
+			writeln("hdr.wafer_id = ", hdr.wafer_id);
+			writeln("hdr.lot_id = ", hdr.lot_id);
+			writeln("hdr.sublot_id = ", hdr.sublot_id);
+			writeln("hdr.devName = ", hdr.devName);
+			writeln("hdr.temperature = ", hdr.temperature);
+			writeln("hdr.step = ", hdr.step);
+			writeln("row = ", row);
+			writeln("col = ", col);
+			writeln("good bins = ", goodbins);
+			writeln("bad bins = ", badbins);
+			writeln("total bins = ", goodbins+badbins);
+		}
 
 		const double colWidth = 2.0;
 		const double rowWidth = 11.6;
@@ -196,7 +175,7 @@ public void genWafermap(CmdOptions options, StdfDB stdfdb, Config config)
 		ws.setRow(cast(uint)(offset_row), rowWidth);			// to include row numbering
 		ws.setRow(cast(uint)(row + offset_row + 1), rowWidth);		// +1 to include other-side row numbering
 
-		foreach(i, row_arr; matrix) {
+		foreach(i, row_arr; matrix_uint) {
 
 			ws.setRow(cast(uint)(i + offset_row + 1), rowWidth);		// -> 0.28"; set rows for all the bin squares
 
@@ -209,31 +188,28 @@ public void genWafermap(CmdOptions options, StdfDB stdfdb, Config config)
 				ws.write(cast(uint)(row + offset_row + 1), cast(ushort)(j + offset_col + 1), j, waferColNumberFmt); // write column numbers on other side
 
 				switch(val) {
-					case 0xFF:
-					case '.':
-						ws.write(cast(uint)(i + offset_row +1), cast(ushort)(j + offset_col+1), to!string(val), waferEmptyFmt);	//+1 for row,col numbering
+					case 0:
+						ws.write(cast(uint)(i + offset_row +1), cast(ushort)(j + offset_col+1), to!int(val), waferEmptyFmt);	//+1 for row,col numbering
+						if(options.asciiDump) { write("."); }
 						break;
-					case '1':
+					case 1:
 						ws.write(cast(uint)(i + offset_row +1), cast(ushort)(j + offset_col+1), to!int(val), waferPassFmt);
+						if(options.asciiDump) { write("1"); }
 						break;
-					case 'X':
-						ws.write(cast(uint)(i + offset_row +1), cast(ushort)(j + offset_col+1), to!string(val), waferFailFmt);
-						break;
-					case '?':
-						ws.write(cast(uint)(i + offset_row +1), cast(ushort)(j + offset_col+1), to!string(val), waferFailFmt);
+					case 2:
+						ws.write(cast(uint)(i + offset_row +1), cast(ushort)(j + offset_col+1), to!int(val), waferFailFmt);
+						if(options.asciiDump) { write("X"); }
 						break;
 					default:
 						throw new Exception("Unknown bin numbering - shouldn't happen");
 				}
 
 				// TO DO:
-				// don't write '.'s.. maybe color gray
-				// combine with prev for loop to optimize time
 				// add die color legend
 				// !!: header location with respect to logo WILL change with different wafer sizes, due to changing row/col size
 
-				// convert char[][] to uint[][] -> easier to output, but harder to dump as ASY?
 			}
+			if(options.asciiDump) { write("\n"); }
 		}
 
 		wb.close();
