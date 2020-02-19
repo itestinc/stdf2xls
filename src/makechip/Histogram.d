75dee614de4156ae212b0de9895630776d6c77a0
Jane Han <jane.han@itestinc.com>
2020-02-18 17:26:12 -0800

    Wafermap support up to 15 hard bins

--- src/makechip/Histogram.d
+++ src/makechip/Histogram.d
@@ -23,6 +23,72 @@ import libxlsxd.chartseries;
 public void genHistogram(CmdOptions options, StdfDB stdfdb, Config config)
 {
 
+/* parametric tests to plot:
+Series -> site
+Categories -> steps/temperatures
+Values -> parametric test results
+*/
+    foreach(hdr; stdfdb.deviceMap.keys) {
+
+        import std.algorithm: canFind;
+        string hfile = options.hfile;	// "<device>_historgrams.pdf"
+        const bool separateFileForDevice = canFind(hfile, "<device>");
+
+        import std.array : replace;
+        string fname = replace(hfile, "<device>", hdr.devName);
+
+        if(separateFileForDevice) {
+            import std.array : replace;
+            fname = replace(hfile, "<device>", hdr.devName);
+            writeln("fname = ", fname);
+        }
+        else {
+            // ...
+        }
+
+        string sheet1 = "Sheet1";
+        Workbook wb = newWorkbook(hfile);
+        Worksheet ws = wb.addWorksheet(sheet1);
+
+        Chart ch = wb.addChart(LXW_CHART_COLUMN);
+        ch.titleSetName("Chart Title");
+
+        // shared categories
+        string categories = "=Sheet1!$A$1:$A$9";
+        const uint categories_firstRow = 0;
+        const uint categories_lastRow = 11;
+        const ushort categories_firstCol = 0;
+        const ushort categories_lastCol = categories_firstCol;
+
+        // series & values 1
+        string values1 = "=Sheet1!$B$1:B$9";
+        Chartseries series1 = ch.addChartseries(categories, values1);
+        const uint values1_firstRow = 0;
+        const uint values1_lastRow = 11;
+        const ushort values1_firstCol = 0;
+        const ushort values1_lastCol = values1_firstCol;
+        series1.setValues(sheet1, values1_firstRow, values1_firstCol, values1_lastRow, values1_lastCol);
+        series1.setName("Series One");
+        series1.setCategories(sheet1, categories_firstRow, categories_firstCol, categories_lastRow, categories_lastCol);
+
+        // series & values 2
+        string values2 = "=Sheet1!$C$1:C$9";
+        Chartseries series2 = ch.addChartseries(categories, values2);
+        const uint values2_firstRow = 0;
+        const uint values2_lastRow = 11;
+        const ushort values2_firstCol = 0;
+        const ushort values2_lastCol = values1_firstCol;
+        series2.setValues(sheet1, values2_firstRow, values2_firstCol, values2_lastRow, values2_lastCol);
+        series2.setName("Series Two");
+        series2.setCategories(sheet1, categories_firstRow, categories_firstCol, categories_lastRow, categories_lastCol);
+
+        // insert chart
+        const uint row = 5;
+        const uint col = 5;
+        ws.insertChart(row, col, ch);
+
+        wb.close();
+    }
 }
 
 unittest {
@@ -52,17 +118,16 @@ unittest {
     ch.titleSetName("Example Title");
 
     // APPLE
-    Chartseries series = ch.addChartseries("=Sheet1!$A$1:$A$9", "=Sheet1!$A$1:A$9"); // categories, values
+    Chartseries series = ch.addChartseries("=Sheet1!$A$1:$A$9", "=Sheet1!$B$1:B$9"); // categories, values
     series.setValues("Sheet1", 1, 1, 8, 1);
     series.setName("=Sheet1!$B$1"); // apple
     series.setCategories("Sheet1", 1, 0, 8, 0);  // same: a, b, c..
 
     // ORANGE
-    Chartseries series2 = ch.addChartseries("=Sheet1!$A$1:$A$9", "=Sheet1!$B$1:B$9");
+    Chartseries series2 = ch.addChartseries("=Sheet1!$A$1:$A$9", "=Sheet1!$C$1:C$9");
     series2.setValues("Sheet1", 1, 2, 8, 2);
     series2.setName("=Sheet1!$C$1"); // orange
     series2.setCategories("Sheet1", 1, 0, 8, 0);  // same: a, b, c.. 
-    
 
     ws.insertChart(10, 2, ch);
 
--- src/makechip/WafermapFormat.d
+++ src/makechip/WafermapFormat.d
@@ -1,3 +1,4 @@
+module makechip.WafermapFormat;
 import libxlsxd.format;
 import libxlsxd.workbook;
 import makechip.CmdOptions;
@@ -12,6 +13,23 @@ static Format waferEmptyFmt;
 static Format waferRowNumberFmt;
 static Format waferColNumberFmt;
 
+static Format waferBin01Fmt;
+static Format waferBin02Fmt;
+static Format waferBin03Fmt;
+static Format waferBin04Fmt;
+static Format waferBin05Fmt;
+static Format waferBin06Fmt;
+static Format waferBin07Fmt;
+static Format waferBin08Fmt;
+static Format waferBin09Fmt;
+static Format waferBin10Fmt;
+static Format waferBin11Fmt;
+static Format waferBin12Fmt;
+static Format waferBin13Fmt;
+static Format waferBin14Fmt;
+static Format waferBin15Fmt;
+static Format waferBin16Fmt;
+
 public void initWaferFormats(Workbook wb, CmdOptions options, Config config)
 {
     if (options.verbosityLevel > 9) writeln("initWaferFormats()");
@@ -24,9 +42,6 @@ public void initWaferFormats(Workbook wb, CmdOptions options, Config config)
     config.setBGColor(headerNameFmt, Config.wafer_header_bg_color);
     config.setFontColor(headerNameFmt, Config.wafer_header_text_color);
     headerNameFmt.setAlign(lxw_format_alignments.LXW_ALIGN_RIGHT);
-    //headerNameFmt.setRight(lxw_format_borders.LXW_BORDER_THIN);
-    headerNameFmt.setBorderColor(0x1000000);
-    //headerNameFmt.setBottom(lxw_format_borders.LXW_BORDER_THIN);
 
     headerValueFmt = wb.addFormat();
     headerValueFmt.setFontName("Arial");
@@ -36,7 +51,6 @@ public void initWaferFormats(Workbook wb, CmdOptions options, Config config)
     headerValueFmt.setAlign(lxw_format_alignments.LXW_ALIGN_LEFT);
     headerValueFmt.setRight(lxw_format_borders.LXW_BORDER_THIN);
     headerValueFmt.setBorderColor(0x1000000);
-    //headerValueFmt.setBottom(lxw_format_borders.LXW_BORDER_THIN);
 
     waferPassFmt = wb.addFormat();
     waferPassFmt.setFontName("Arial");
@@ -92,4 +106,182 @@ public void initWaferFormats(Workbook wb, CmdOptions options, Config config)
     waferColNumberFmt.setRotation(90);
     waferColNumberFmt.setRight(lxw_format_borders.LXW_BORDER_THIN);
     waferColNumberFmt.setBorderColor(0x1000000);
+
+    waferBin01Fmt = wb.addFormat();
+    waferBin01Fmt.setFontName("Arial");
+    waferBin01Fmt.setFontSize(7.0);
+    config.setBGColor(waferBin01Fmt, Config.wafer_bin01_bg_color);
+    config.setFontColor(waferBin01Fmt, Config.wafer_pass_text_color);
+    waferBin01Fmt.setAlign(lxw_format_alignments.LXW_ALIGN_CENTER);
+    waferBin01Fmt.setAlign(lxw_format_alignments.LXW_ALIGN_VERTICAL_CENTER);
+    waferBin01Fmt.setRight(lxw_format_borders.LXW_BORDER_THIN);
+    waferBin01Fmt.setBorderColor(0x1000000);
+    waferBin01Fmt.setBottom(lxw_format_borders.LXW_BORDER_THIN);
+    //waferBin01Fmt.setTop(lxw_format_borders.LXW_BORDER_THIN);
+    //waferBin01Fmt.setLeft(lxw_format_borders.LXW_BORDER_THIN);
+
+    waferBin02Fmt = wb.addFormat();
+    waferBin02Fmt.setFontName("Arial");
+    waferBin02Fmt.setFontSize(7.0);
+    config.setBGColor(waferBin02Fmt, Config.wafer_bin02_bg_color);
+    config.setFontColor(waferBin02Fmt, Config.wafer_pass_text_color);
+    waferBin02Fmt.setAlign(lxw_format_alignments.LXW_ALIGN_CENTER);
+    waferBin02Fmt.setAlign(lxw_format_alignments.LXW_ALIGN_VERTICAL_CENTER);
+    waferBin02Fmt.setRight(lxw_format_borders.LXW_BORDER_THIN);
+    waferBin02Fmt.setBorderColor(0x1000000);
+    waferBin02Fmt.setBottom(lxw_format_borders.LXW_BORDER_THIN);
+
+    waferBin03Fmt = wb.addFormat();
+    waferBin03Fmt.setFontName("Arial");
+    waferBin03Fmt.setFontSize(7.0);
+    config.setBGColor(waferBin03Fmt, Config.wafer_bin03_bg_color);
+    config.setFontColor(waferBin03Fmt, Config.wafer_pass_text_color);
+    waferBin03Fmt.setAlign(lxw_format_alignments.LXW_ALIGN_CENTER);
+    waferBin03Fmt.setAlign(lxw_format_alignments.LXW_ALIGN_VERTICAL_CENTER);
+    waferBin03Fmt.setRight(lxw_format_borders.LXW_BORDER_THIN);
+    waferBin03Fmt.setBorderColor(0x1000000);
+    waferBin03Fmt.setBottom(lxw_format_borders.LXW_BORDER_THIN);
+
+    waferBin04Fmt = wb.addFormat();
+    waferBin04Fmt.setFontName("Arial");
+    waferBin04Fmt.setFontSize(7.0);
+    config.setBGColor(waferBin04Fmt, Config.wafer_bin04_bg_color);
+    config.setFontColor(waferBin04Fmt, Config.wafer_pass_text_color);
+    waferBin04Fmt.setAlign(lxw_format_alignments.LXW_ALIGN_CENTER);
+    waferBin04Fmt.setAlign(lxw_format_alignments.LXW_ALIGN_VERTICAL_CENTER);
+    waferBin04Fmt.setRight(lxw_format_borders.LXW_BORDER_THIN);
+    waferBin04Fmt.setBorderColor(0x1000000);
+    waferBin04Fmt.setBottom(lxw_format_borders.LXW_BORDER_THIN);
+
+    waferBin05Fmt = wb.addFormat();
+    waferBin05Fmt.setFontName("Arial");
+    waferBin05Fmt.setFontSize(7.0);
+    config.setBGColor(waferBin05Fmt, Config.wafer_bin05_bg_color);
+    config.setFontColor(waferBin05Fmt, Config.wafer_pass_text_color);
+    waferBin05Fmt.setAlign(lxw_format_alignments.LXW_ALIGN_CENTER);
+    waferBin05Fmt.setAlign(lxw_format_alignments.LXW_ALIGN_VERTICAL_CENTER);
+    waferBin05Fmt.setRight(lxw_format_borders.LXW_BORDER_THIN);
+    waferBin05Fmt.setBorderColor(0x1000000);
+    waferBin05Fmt.setBottom(lxw_format_borders.LXW_BORDER_THIN);
+
+    waferBin06Fmt = wb.addFormat();
+    waferBin06Fmt.setFontName("Arial");
+    waferBin06Fmt.setFontSize(7.0);
+    config.setBGColor(waferBin06Fmt, Config.wafer_bin06_bg_color);
+    config.setFontColor(waferBin06Fmt, Config.wafer_pass_text_color);
+    waferBin06Fmt.setAlign(lxw_format_alignments.LXW_ALIGN_CENTER);
+    waferBin06Fmt.setAlign(lxw_format_alignments.LXW_ALIGN_VERTICAL_CENTER);
+    waferBin06Fmt.setRight(lxw_format_borders.LXW_BORDER_THIN);
+    waferBin06Fmt.setBorderColor(0x1000000);
+    waferBin06Fmt.setBottom(lxw_format_borders.LXW_BORDER_THIN);
+
+    waferBin07Fmt = wb.addFormat();
+    waferBin07Fmt.setFontName("Arial");
+    waferBin07Fmt.setFontSize(7.0);
+    config.setBGColor(waferBin07Fmt, Config.wafer_bin07_bg_color);
+    config.setFontColor(waferBin07Fmt, Config.wafer_pass_text_color);
+    waferBin07Fmt.setAlign(lxw_format_alignments.LXW_ALIGN_CENTER);
+    waferBin07Fmt.setAlign(lxw_format_alignments.LXW_ALIGN_VERTICAL_CENTER);
+    waferBin07Fmt.setRight(lxw_format_borders.LXW_BORDER_THIN);
+    waferBin07Fmt.setBorderColor(0x1000000);
+    waferBin07Fmt.setBottom(lxw_format_borders.LXW_BORDER_THIN);
+
+    waferBin08Fmt = wb.addFormat();
+    waferBin08Fmt.setFontName("Arial");
+    waferBin08Fmt.setFontSize(7.0);
+    config.setBGColor(waferBin08Fmt, Config.wafer_bin08_bg_color);
+    config.setFontColor(waferBin08Fmt, Config.wafer_pass_text_color);
+    waferBin08Fmt.setAlign(lxw_format_alignments.LXW_ALIGN_CENTER);
+    waferBin08Fmt.setAlign(lxw_format_alignments.LXW_ALIGN_VERTICAL_CENTER);
+    waferBin08Fmt.setRight(lxw_format_borders.LXW_BORDER_THIN);
+    waferBin08Fmt.setBorderColor(0x1000000);
+    waferBin08Fmt.setBottom(lxw_format_borders.LXW_BORDER_THIN);
+
+    waferBin09Fmt = wb.addFormat();
+    waferBin09Fmt.setFontName("Arial");
+    waferBin09Fmt.setFontSize(7.0);
+    config.setBGColor(waferBin09Fmt, Config.wafer_bin09_bg_color);
+    config.setFontColor(waferBin09Fmt, Config.wafer_pass_text_color);
+    waferBin09Fmt.setAlign(lxw_format_alignments.LXW_ALIGN_CENTER);
+    waferBin09Fmt.setAlign(lxw_format_alignments.LXW_ALIGN_VERTICAL_CENTER);
+    waferBin09Fmt.setRight(lxw_format_borders.LXW_BORDER_THIN);
+    waferBin09Fmt.setBorderColor(0x1000000);
+    waferBin09Fmt.setBottom(lxw_format_borders.LXW_BORDER_THIN);
+
+    waferBin10Fmt = wb.addFormat();
+    waferBin10Fmt.setFontName("Arial");
+    waferBin10Fmt.setFontSize(7.0);
+    config.setBGColor(waferBin10Fmt, Config.wafer_bin10_bg_color);
+    config.setFontColor(waferBin10Fmt, Config.wafer_pass_text_color);
+    waferBin10Fmt.setAlign(lxw_format_alignments.LXW_ALIGN_CENTER);
+    waferBin10Fmt.setAlign(lxw_format_alignments.LXW_ALIGN_VERTICAL_CENTER);
+    waferBin10Fmt.setRight(lxw_format_borders.LXW_BORDER_THIN);
+    waferBin10Fmt.setBorderColor(0x1000000);
+    waferBin10Fmt.setBottom(lxw_format_borders.LXW_BORDER_THIN);
+
+    waferBin11Fmt = wb.addFormat();
+    waferBin11Fmt.setFontName("Arial");
+    waferBin11Fmt.setFontSize(7.0);
+    config.setBGColor(waferBin11Fmt, Config.wafer_bin11_bg_color);
+    config.setFontColor(waferBin11Fmt, Config.wafer_pass_text_color);
+    waferBin11Fmt.setAlign(lxw_format_alignments.LXW_ALIGN_CENTER);
+    waferBin11Fmt.setAlign(lxw_format_alignments.LXW_ALIGN_VERTICAL_CENTER);
+    waferBin11Fmt.setRight(lxw_format_borders.LXW_BORDER_THIN);
+    waferBin11Fmt.setBorderColor(0x1000000);
+    waferBin11Fmt.setBottom(lxw_format_borders.LXW_BORDER_THIN);
+
+    waferBin12Fmt = wb.addFormat();
+    waferBin12Fmt.setFontName("Arial");
+    waferBin12Fmt.setFontSize(7.0);
+    config.setBGColor(waferBin12Fmt, Config.wafer_bin12_bg_color);
+    config.setFontColor(waferBin12Fmt, Config.wafer_pass_text_color);
+    waferBin12Fmt.setAlign(lxw_format_alignments.LXW_ALIGN_CENTER);
+    waferBin12Fmt.setAlign(lxw_format_alignments.LXW_ALIGN_VERTICAL_CENTER);
+    waferBin12Fmt.setRight(lxw_format_borders.LXW_BORDER_THIN);
+    waferBin12Fmt.setBorderColor(0x1000000);
+    waferBin12Fmt.setBottom(lxw_format_borders.LXW_BORDER_THIN);
+
+    waferBin13Fmt = wb.addFormat();
+    waferBin13Fmt.setFontName("Arial");
+    waferBin13Fmt.setFontSize(7.0);
+    config.setBGColor(waferBin13Fmt, Config.wafer_bin13_bg_color);
+    config.setFontColor(waferBin13Fmt, Config.wafer_pass_text_color);
+    waferBin13Fmt.setAlign(lxw_format_alignments.LXW_ALIGN_CENTER);
+    waferBin13Fmt.setAlign(lxw_format_alignments.LXW_ALIGN_VERTICAL_CENTER);
+    waferBin13Fmt.setRight(lxw_format_borders.LXW_BORDER_THIN);
+    waferBin13Fmt.setBorderColor(0x1000000);
+    waferBin13Fmt.setBottom(lxw_format_borders.LXW_BORDER_THIN);
+
+    waferBin14Fmt = wb.addFormat();
+    waferBin14Fmt.setFontName("Arial");
+    waferBin14Fmt.setFontSize(7.0);
+    config.setBGColor(waferBin14Fmt, Config.wafer_bin14_bg_color);
+    config.setFontColor(waferBin14Fmt, Config.wafer_pass_text_color);
+    waferBin14Fmt.setAlign(lxw_format_alignments.LXW_ALIGN_CENTER);
+    waferBin14Fmt.setAlign(lxw_format_alignments.LXW_ALIGN_VERTICAL_CENTER);
+    waferBin14Fmt.setRight(lxw_format_borders.LXW_BORDER_THIN);
+    waferBin14Fmt.setBorderColor(0x1000000);
+    waferBin14Fmt.setBottom(lxw_format_borders.LXW_BORDER_THIN);
+
+    waferBin15Fmt = wb.addFormat();
+    waferBin15Fmt.setFontName("Arial");
+    waferBin15Fmt.setFontSize(7.0);
+    config.setBGColor(waferBin15Fmt, Config.wafer_bin15_bg_color);
+    config.setFontColor(waferBin15Fmt, Config.wafer_pass_text_color);
+    waferBin15Fmt.setAlign(lxw_format_alignments.LXW_ALIGN_CENTER);
+    waferBin15Fmt.setAlign(lxw_format_alignments.LXW_ALIGN_VERTICAL_CENTER);
+    waferBin15Fmt.setRight(lxw_format_borders.LXW_BORDER_THIN);
+    waferBin15Fmt.setBorderColor(0x1000000);
+    waferBin15Fmt.setBottom(lxw_format_borders.LXW_BORDER_THIN);
+
+    waferBin16Fmt = wb.addFormat();
+    waferBin16Fmt.setFontName("Arial");
+    waferBin16Fmt.setFontSize(7.0);
+    config.setBGColor(waferBin16Fmt, Config.wafer_bin16_bg_color);
+    config.setFontColor(waferBin16Fmt, Config.wafer_pass_text_color);
+    waferBin16Fmt.setAlign(lxw_format_alignments.LXW_ALIGN_CENTER);
+    waferBin16Fmt.setAlign(lxw_format_alignments.LXW_ALIGN_VERTICAL_CENTER);
+    waferBin16Fmt.setRight(lxw_format_borders.LXW_BORDER_THIN);
+    waferBin16Fmt.setBorderColor(0x1000000);
+    waferBin16Fmt.setBottom(lxw_format_borders.LXW_BORDER_THIN);
 }
